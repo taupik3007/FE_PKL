34cdd0ab69014271d63f00ed202e11251f6bfeac
taupik3007 <taupikpathurrohman@gmail.com>
2024-02-04 20:38:26 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  
+  theme: {
+    extend: {
+      fontFamily:{
+        'poppins' : ['Poppins','sanserif']
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
+
